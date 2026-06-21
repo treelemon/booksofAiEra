5db025cae7a4e5a0388d6d333309b8349ee50568
treelemon <treelemon@users.noreply.github.com>
2026-06-21 14:32:03 +0000

    deploy: 8b8cf829ae4085c733a2f48f7ddfd92cf894c63b

--- zh/searcher-09f2665d.js
+++ zh/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c4f08b69.js',
+                path_to_root + 'searchindex-4ef70648.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
