09d7d2d7a7793c2e3276afa2b7ad8b115398b349
treelemon <treelemon@users.noreply.github.com>
2026-06-21 10:45:52 +0000

    deploy: 719d0738a5c3aa46389b09ca4200afda8884021e

--- zh/searcher-09f2665d.js
+++ zh/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0fa19c70.js',
+                path_to_root + 'searchindex-2d0b609d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
