6998f0325e649fe0bf24e211eefc7c7fa8626d21
treelemon <treelemon@users.noreply.github.com>
2026-07-31 17:17:50 +0000

    deploy: f49640a5c8597ae433dc1af8cb51a7106ce62d0f

--- silicon-blockade/zh/searcher-09f2665d.js
+++ silicon-blockade/zh/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e38b7e32.js',
+                path_to_root + 'searchindex-9ec1b9a4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
