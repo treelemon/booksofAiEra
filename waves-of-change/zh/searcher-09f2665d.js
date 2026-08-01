ad4482ca66a01bf902cf2df1a5f108362997c3e1
treelemon <treelemon@users.noreply.github.com>
2026-08-01 05:34:27 +0000

    deploy: 15b62e47a783c9e9be0784f0d7fc0d267ed413b6

--- waves-of-change/zh/searcher-09f2665d.js
+++ waves-of-change/zh/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b8ae3c9d.js',
+                path_to_root + 'searchindex-c5afa064.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
